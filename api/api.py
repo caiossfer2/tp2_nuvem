64dfdac799b085c7ff9354c7d6a668098b843ac5
caio <caiossfer2@gmail.com>
2024-12-22 10:41:55 -0300

    primeiro

---
+++ api/api.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify
+import pickle
+from datetime import date
+
+MIN_SUPPORT_THRESHOLD = 0.6
+CODE_VERSION = 1.0
+SONGS_NUMBER = 7
+
+app = Flask(__name__)
+app.model = pickle.load(open("recommendation_model.pkl", "rb"))
+
+def filter_rules_by_confidence(rules, min_confidence):
+    filtered_rules = [rule for rule in rules if rule[2] >= min_confidence]
+    return filtered_rules
+
+def get_recommendations(user_tracks, num_recommendations=5):
+    rules, track_counts, _ = app.model
+
+    filtered_rules = filter_rules_by_confidence(rules, MIN_SUPPORT_THRESHOLD)
+    recommendations = set()
+
+    # Tentar gerar recomendações baseadas em regras
+    for rule in filtered_rules:
+        antecedents = rule[0]
+        consequents = rule[1]
+        
+        if antecedents.issubset(user_tracks):
+            recommendations.update(consequents)
+
+    # Remover faixas já ouvidas das recomendações
+    recommendations.difference_update(user_tracks)
+
+    # Se não houver recomendações suficientes, usar fallback de popularidade
+    if len(recommendations) < num_recommendations:
+        most_common_tracks = [track for track, _ in track_counts.most_common() if track not in user_tracks]
+        additional_recommendations = most_common_tracks[:num_recommendations - len(recommendations)]
+        recommendations.update(additional_recommendations)
+
+    return list(recommendations)[:num_recommendations]
+
+
+
+@app.route("/api/recommend", methods=["POST"])
+def recommend():
+    request_data = request.json
+    songs_set = set(request_data["songs"])
+    _, _, last_update_date = app.model
+    return jsonify(
+        {
+            "songs": get_recommendations(songs_set, SONGS_NUMBER),
+            "version": CODE_VERSION,
+            "model_date": last_update_date
+        }
+    )
+
+ 
+
+# flask --app api.py run --port 52008
+
+
+
+
+
+#  @app.route("/api/recommend", methods=["POST"])
+# def recommend():
+#     request_data = request.json
+#     songs_set = set(request_data["songs"])
+#     _, _, last_update_date = app.model
+#     return jsonify(
+#         {
+#             "songs": get_recommendations(songs_set, request_data['songs_number']),
+#             "version": CODE_VERSION,
+#             "model_date": last_update_date
+#         }
+#     )
---
+++ ml_model/playlist_rules_generator.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from fpgrowth_py import fpgrowth
+import pickle
+from collections import Counter
+from datetime import datetime
+
+
+MIN_SUPPORT = 0.06  
+MIN_CONFIDENCE = 0.5  
+
+
+def train_model():
+    data = pd.read_csv('2023_spotify_ds1.csv')
+    transactions = data.groupby('pid')['track_name'].apply(list).tolist()
+    _, rules = fpgrowth(transactions, minSupRatio=MIN_SUPPORT, minConf=MIN_CONFIDENCE)
+    # Calcular a popularidade das faixas
+    all_tracks = data['track_name'].tolist()
+    track_counts = Counter(all_tracks)
+    last_update_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open('recommendation_model.pkl', 'wb') as f:
+        pickle.dump((rules, track_counts, last_update_date), f)
+
+
+train_model()
+
