c1b0288850a328a4cb69346206bb87224e6bd4ad
caio <caiossfer2@gmail.com>
2024-12-26 09:28:00 -0300

    arquivo de deploy unico

--- ml_model/playlist_rules_generator.py
+++ ml_model/playlist_rules_generator.py
@@ -9,16 +9,19 @@ import os
 MIN_SUPPORT = 0.06  
 MIN_CONFIDENCE = 0.5  
 
+url = os.getenv("DATASET_URL")
+
 model_path = "/app/model/recommendation_model.pkl"
 
 os.makedirs(os.path.dirname(model_path), exist_ok=True)
 
 def train_model():
-    data = pd.read_csv('2023_spotify_ds1.csv')
-    transactions = data.groupby('pid')['track_name'].apply(list).tolist()
+    print('executando')
+    df = pd.read_csv(url)
+    transactions = df.groupby('pid')['track_name'].apply(list).tolist()
     _, rules = fpgrowth(transactions, minSupRatio=MIN_SUPPORT, minConf=MIN_CONFIDENCE)
     # Calcular a popularidade das faixas
-    all_tracks = data['track_name'].tolist()
+    all_tracks = df['track_name'].tolist()
     track_counts = Counter(all_tracks)
     last_update_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     with open(model_path, 'wb') as f:
